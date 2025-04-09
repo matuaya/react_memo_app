31b1b49f7bed4be85eb6f9594f7e9a21ac5caca7
matsuyama <aya.k.matsuyama@gmail.com>
2025-04-09 17:36:23 +0900

    メモアプリのロジックをMemoAppコンポーネントに移動

--- memo-app/src/App.jsx
+++ memo-app/src/App.jsx
@@ -1,76 +1,10 @@
-import { useState } from "react";
-import MemoList from "./components/MemoList.jsx";
-import Form from "./components/Form.jsx";
-import useLocalStorage from "./hooks/useLocalStorage.jsx";
+import MemoApp from "./components/MemoApp.jsx";
 
 function App() {
-  const [memos, setMemos] = useLocalStorage("memo");
-  const [isAdding, setIsAdding] = useState(false);
-  const [selectedId, setSelectedId] = useState(null);
-  const selectedMemo = memos.find((memo) => memo.id === selectedId);
-
-  function handleClickMemo(id) {
-    setSelectedId(id);
-  }
-
-  function handleClickAdd() {
-    setIsAdding(true);
-    setSelectedId(null);
-  }
-
-  function handleSave(content) {
-    const newId = crypto.randomUUID();
-
-    if (isAdding) {
-      setMemos([
-        ...memos,
-        {
-          id: newId,
-          content,
-        },
-      ]);
-    } else {
-      const modifiedMemos = memos.map((memo) =>
-        memo.id === selectedId
-          ? {
-              ...memo,
-              content,
-            }
-          : memo
-      );
-      setMemos(modifiedMemos);
-    }
-
-    setIsAdding(false);
-    setSelectedId(null);
-  }
-
-  function handleDelete() {
-    if (selectedId) {
-      setMemos(memos.filter((memo) => memo.id !== selectedId));
-    }
-
-    setIsAdding(false);
-    setSelectedId(null);
-  }
-
   return (
-    <div className="wrapper">
-      <MemoList
-        memos={memos}
-        onClickMemo={handleClickMemo}
-        onClickAdd={handleClickAdd}
-      />
-      {(isAdding || selectedId) && (
-        <Form
-          key={selectedId || "new"}
-          memo={selectedMemo}
-          onSave={handleSave}
-          onDelete={handleDelete}
-          isAdding={isAdding}
-        />
-      )}
-    </div>
+    <>
+      <MemoApp />
+    </>
   );
 }
 
